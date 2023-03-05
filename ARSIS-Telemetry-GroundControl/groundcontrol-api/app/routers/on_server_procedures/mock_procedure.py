f2c9681d0aca55708b49cc203f8c122371c0ccbe
chuck <c.burnell@gmail.com>
2023-03-04 20:13:11 -0700

    making stuff less bad

--- ARSIS-Telemetry-GroundControl/groundcontrol-api/app/routers/on_server_procedures/mock_procedure.py
+++ ARSIS-Telemetry-GroundControl/groundcontrol-api/app/routers/on_server_procedures/mock_procedure.py
@@ -4,20 +4,37 @@ import base64
 class MockProcedure:
     def __init__(self):
         self.name = "Mock Procedure"
-        suits_image = base64.b64encode(open("app/routers/on_server_procedures/NASASUITS-logo.png", "rb").read())
-        self.task_list = [
-            ["text", "asdfasdf asdfasdf asdf sadf asdf asdf"],
-            [
-                "text",
-                "lkashdflkasdfdffdfdfkasjfdaadsfsfasdfasdfasdfasoiueoriuwoeirtn,msnd,gmnsdf,gns,dnfgm,sdnf,msndjfksiuoqweuioqweuioxcbncxvbncxvbnxcvbnaghfasdfgsadwqyetwty32463278sdgusd",
-            ],
-            ["text", "asdf"],
-            [
-                "text",
-                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            ],
-            ["image", suits_image],
+
+        suits_image = base64.b64encode(
+            open("app/routers/on_server_procedures/NASASUITS-logo.png", "rb").read()
+        )
+        task_1_list = [
+            {"type": "image", "body": suits_image, "next_step": ""},
+            {
+                "type": "text",
+                "body": "do this next thing and if it works goto otherwise",
+                "next_steps": {"procedure": "Mock Procedure", "step": 2},
+            },
         ]
+        task_1 = {
+            "name": "Name of Task",
+            "summary": "Summary of what task is meant to do",
+            "list": task_1_list,
+        }
+
+        task_2_list = [
+            {
+                "type": "text",
+                "body": "2nd step of mock procedure",
+                "next_steps": {"procedure": "Idle Procedure", "step": 0},
+            },
+        ]
+        task_2 = {
+            "name": "Name of Task",
+            "summary": "Summary of what task is meant to do",
+            "list": task_2_list,
+        }
+        self.task_list = [task_1, task_2]
 
     def get_name(self):
         return self.name
@@ -26,9 +43,4 @@ class MockProcedure:
         return self.task_list
 
     def get_task_list_encoded(self):
-        to_ret = [
-            str.encode(s) if type(s) is str else s
-            for tup in self.task_list
-            for s in tup
-        ]
-        return to_ret
+        return self.task_list
